2551eaf4e02277934a972bffe3f17f45c3931064
BYSANI VENKATA SUBBASAI HARISHGUPTHA <113228409+HarishGuptha9797@users.noreply.github.com>
2024-01-23 19:02:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import {useEffect, useState} from 'react';
+import MovieCard from './MovieCard';
+import './App.css';
+import SearchIcon from './search.svg'
+//1f9f0b6c
+const API_URL= 'https://www.omdbapi.com?apikey=1f9f0b6c';
+
+
+const App = ()=> {
+    const [movies,setMovies]=useState([]);
+    const [searchTerm , setsearchTerm] = useState('');
+
+    const searchMovies= async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+        
+        setMovies(data.Search);
+    }
+    useEffect(()=> {
+        searchMovies('rrr');
+    },[]);
+    return(
+        <div className='app'>
+            <h1>MovieWorld</h1>
+            <div className='search'>
+            <input 
+            placeholder='Search for movies' 
+            value={searchTerm}
+            onChange={(e)=>setsearchTerm(e.target.value)}
+            onKeyDown={ (e) =>{ if(e.key==='Enter') {searchMovies(searchTerm)}}}
+            />
+            <img 
+                src={SearchIcon}
+                alt="search"
+                onClick={()=>{searchMovies(searchTerm)}}
+                
+            />
+            </div>
+            
+            {
+                movies?.length>0 // if len is 0 then it will go fo false case
+                ? (
+
+                    <div className='container'>
+                        {movies.map((movie)=> (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                ) :
+                (
+                    <div className='empty'>
+                        <h2> No movies found</h2>
+                    </div>
+                )
+            }
+
+        </div>
+    );
+}
+
+export default App;
